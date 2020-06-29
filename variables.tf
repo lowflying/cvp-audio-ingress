16a8a53070db5cddeb02008357e983be29ea3855
Daniel Ablett <danielablett@jango-fett.local>
2020-06-29 14:26:33 +0100

    dynatrace install

--- variables.tf
+++ variables.tf
@@ -94,3 +94,15 @@ variable "ws_rg" {
 variable "ws_sub_id" {
   type = string
 }
+
+variable "dynatrace_id" {
+  type = string
+}
+
+variable "dynatrace_host_group" {
+  type = string
+}
+
+variable "dynatrace_network_zone" {
+  type = string
+}
