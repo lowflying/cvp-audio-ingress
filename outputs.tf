ded0a54891bc76bee7b923cf5b7482565aa11b32
lowflying <lowflyingnow@gmail.com>
2021-05-06 14:05:23 +0100

    marked password outs as sensitive

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,13 @@
 output "rest_password" {
   description = ""
   value       = module.wowza.rest_password
+  sensitive    = true
 }
 
 output "stream_password" {
   description = ""
   value       = module.wowza.stream_password
+  sensitive    = true
 }
 
 # output "lb_pip" {
